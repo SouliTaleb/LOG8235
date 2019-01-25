8d019089d716f6726e9ae7cf912367aa01b55db0
Taleb <taleb.souli.polymtl.ca>
2019-01-25 00:00:48 -0500

    avoid obstacles
    Added rotation movement to avoid obstacles

--- TP1/Source/SoftDesignTraining/SDTAIController.cpp
+++ TP1/Source/SoftDesignTraining/SDTAIController.cpp
@@ -24,15 +24,83 @@ void ASDTAIController::BeginPlay()
 
 void ASDTAIController::Tick(float deltaTime)
 {
-	Move(m_maxAcceleration, m_maxSpeed, deltaTime);
+	switch (m_state)
+	{
+		case State::MoveForward:
+		{
+			FVector const ForwardDirection = GetPawn()->GetActorForwardVector().GetSafeNormal();
+			Move(FVector2D(ForwardDirection), m_maxAcceleration, m_maxSpeed, deltaTime);
+
+			struct FHitResult hitResult;
+			if (RayCast(hitResult))
+			{
+				float distanceToImpactPoint = (hitResult.ImpactPoint - GetPawn()->GetActorLocation()).Size();
+				if (distanceToImpactPoint <= 100.f)
+				{
+					m_state = State::AvoidObstacle;
+				} 
+			}
+		}
+		break;
+		case State::AvoidObstacle:
+		{
+			struct FHitResult hitResult;
+			if (RayCast(hitResult))
+			{
+				m_obstacleInformation.ObstacleDetected(FVector2D(hitResult.Normal));
+				AvoidObstacle(hitResult, deltaTime);
+			}
+			m_state = State::MoveForward;
+		}
+		break;
+		case State::Stop:
+		{
+
+		}
+		break;
+	};
 }
 
-void ASDTAIController::Move(float acceleration, float maxSpeed, float deltaTime)
+void ASDTAIController::Move(const FVector2D& direction, float acceleration, float maxSpeed, float deltaTime)
 {
 	APawn* pawn = GetPawn();
-	FVector const ForwardDirection = pawn->GetActorForwardVector().GetSafeNormal();
 	m_currentSpeed = FMath::Min(maxSpeed, m_currentSpeed + acceleration * deltaTime);
-	pawn->AddMovementInput(ForwardDirection, m_currentSpeed);
-	FRotator deltaRotation = FVector(FVector2D(ForwardDirection).GetSafeNormal(), 0.f).ToOrientationRotator();
-	pawn->AddActorWorldRotation(deltaRotation);
+	pawn->AddMovementInput(FVector(direction, 0.f), m_currentSpeed);
+	FRotator deltaRotation = FVector(direction, 0.f).ToOrientationRotator();
+	pawn->SetActorRotation(FVector(direction, 0.f).ToOrientationQuat());
+}
+
+bool ASDTAIController::RayCast(struct FHitResult& outHit)
+{
+	UWorld * world = GetWorld();
+	if (world == nullptr)
+		return false;
+
+	//Single line trace
+	FCollisionObjectQueryParams objectQueryParams;
+	objectQueryParams.AddObjectTypesToQuery(ECC_PhysicsBody);
+	objectQueryParams.AddObjectTypesToQuery(ECC_WorldStatic);
+	objectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	FCollisionQueryParams queryParams = FCollisionQueryParams::DefaultQueryParam;
+	queryParams.bReturnPhysicalMaterial = true;
+
+	APawn* pawn = GetPawn();
+	FVector const actorForwardDirection = pawn->GetActorForwardVector();
+	FVector const startTrace = pawn->GetActorLocation();
+	FVector const endTrace = actorForwardDirection * 1000.f + startTrace;
+
+	return world->LineTraceSingleByObjectType(outHit, startTrace, endTrace, objectQueryParams, queryParams) 
+		   && (outHit.GetActor() != NULL);
+}
+
+bool ASDTAIController::AvoidObstacle(const struct FHitResult& obstacle, const float deltaTime)
+{
+	float distanceToImpactPoint = 0.f;
+	distanceToImpactPoint = (obstacle.ImpactPoint - GetPawn()->GetActorLocation()).Size();
+	if(distanceToImpactPoint <= 100.f)
+	{
+		FVector2D const newActorDirection = FVector2D(FVector::CrossProduct(FVector::UpVector, FVector(m_obstacleInformation.m_obstacleNormal, 0.0f)));
+		Move(newActorDirection, m_maxAcceleration, m_maxSpeed, deltaTime);
+	}
+	return true; 
 }
--- TP1/Source/SoftDesignTraining/SDTAIController.h
+++ TP1/Source/SoftDesignTraining/SDTAIController.h
@@ -16,18 +16,53 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
     GENERATED_BODY()
 public:
 	ASDTAIController();
-
 	virtual void BeginPlay() override;
     virtual void Tick(float deltaTime) override;
 
 protected:
-	virtual void Move(float acceleration, float maxSpeed, float deltaTime);
+	enum class State
+	{
+		MoveForward,
+		AvoidObstacle,
+		Stop
+	};
 
+	struct ObstacleInformation
+	{
+		void ObstacleDetected(FVector2D obstacleNormal)
+		{
+			m_obstacleNormal = obstacleNormal;
+			m_obstacleAvoided = false;
+		}
+
+		bool AvoidObstacle()
+		{
+			if (!m_obstacleAvoided)
+			{
+				m_obstacleAvoided = true;
+				return true;
+			}
+			return false;
+		}
+
+		bool m_obstacleAvoided = true;
+		FVector2D m_obstacleNormal;
+	};
+
+	void Move(const FVector2D& direction, float acceleration, float maxSpeed, float deltaTime);
+
+private:
+	bool RayCast(struct FHitResult& outHit);
+	bool AvoidObstacle(const struct FHitResult&  outHit, const float deltaTime);
+
+private:
 	FVector2D m_MovementInput;
 	FVector2D m_StartingPosition;
 	float m_capsuleRadius;
 	float m_currentSpeed = 0.0f;
 	float const m_maxSpeed = 500.0f;
 	float const m_maxAcceleration = 500.0f;
+	State m_state = State::MoveForward;
+	ObstacleInformation m_obstacleInformation;
 	float const m_visionAngle = PI / 3.0f;
 };
